dc9e8f7fe8ffd723c60209e9321d41d475cf407c
clempal <clement.palliere@hotmail.fr>
2014-05-06 15:48:28 +0800

    Module ready to be used - version 1.0

--- jitPressureSensor/sensors/pressure_signal.py
+++ jitPressureSensor/sensors/pressure_signal.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import re #why
+import re
 
 from ubiGATE.ubigate.utils.logger import logger
 
@@ -11,38 +11,65 @@ def matches(signal):
     :returns: @todo
 
     """
-    # sample matching input: 01/01 00:14:31 Rx RF HouseUnit: A1 Func: On
-    logger.debug('Checking motion for signal "%s"' % signal)
+    #Chair,2014-05-06 12:55:09.101,0,0,0,0,0,0,0,0
+    logger.debug('Checking pressure for signal "%s"' % signal)
 
-    pattern = (r'^(?P<month>\d{2})/(?P<day>\d{2})\s'
-               '(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})\s'
-               'Rx\sRF\sHouseUnit:\s(?P<sensor>\w\d+)\s'
-               'Func:\s(?P<value>On|Off)$\n*')
+    pattern = (r'^Chair,(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})\s'
+               '(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2}).'
+               '(?P<msecond>\d{3}),(?P<D0>\d+),(?P<D1>\d+),(?P<D2>\d+),'
+               '(?P<D3>\d+),(?P<D4>\d+),(?P<D5>\d+),(?P<D6>\d+)'
+               ',(?P<D7>\d+)$\n*')
     regexp = re.compile(pattern)
 
     match = regexp.match(signal)
 
     if match:
-        logger.info('Motion detected:')
+        logger.info('Pressure detected:')
 
-        try: #why
+        try:
             date = datetime(datetime.now().year,
                             int(match.group('month')),
                             int(match.group('day')),
                             int(match.group('hour')),
                             int(match.group('minute')),
                             int(match.group('second')))
+
         except ValueError:
             logger.warn('Invalid date: %s-%s-%s %s:%s:%s, event skipped'
                         % (datetime.datetime.now().year, match.group('month'),
                            match.group('day'), match.group('hour'),
                            match.group('minute'), match.group('second')))
             return None
-        logger.info('value: "%s", sensor: "%s", date: "%s"' %
-                    (match.group('value'), match.group('sensor'), date))
 
-        data = {'value': match.group('value'),
-                'sensor': match.group('sensor'),
-                'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        logger.info('value of the sensors:\nD0:"%s", D1:"%s", D2:"%s", D3:"%s", D4:"%s", D5:"%s", D6:"%s", D7:"%s", date: "%s"' %
+                    (match.group('D0'), match.group('D1'), match.group('D2'), match.group('D3'), match.group('D4'), match.group('D5'), match.group('D6'), match.group('D7'), date))
+
+        data = {}
+        data['D0'] = {'value': match.group('D0'),
+                      'sensor': 'D0',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D1'] = {'value': match.group('D1'),
+                      'sensor': 'D1',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D2'] = {'value': match.group('D2'),
+                      'sensor': 'D2',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D3'] = {'value': match.group('D3'),
+                      'sensor': 'D3',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D4'] = {'value': match.group('D4'),
+                      'sensor': 'D4',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D5'] = {'value': match.group('D5'),
+                      'sensor': 'D5',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D6'] = {'value': match.group('D6'),
+                      'sensor': 'D6',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+        data['D7'] = {'value': match.group('D7'),
+                      'sensor': 'D7',
+                      'date': date.strftime('%Y-%m-%d %H:%M:%S')}
+
         return data
+
     return None
